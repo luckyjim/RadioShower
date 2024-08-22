a719c785bd126179a3fb843cccb710ea73196738
luckyjim <jcolley@lpnhe.in2p3.fr>
2024-08-22 11:53:54 +0200

    define file name of Leff model

--- src/rshower/io/leff_fmt.py
+++ src/rshower/io/leff_fmt.py
@@ -1,6 +1,6 @@
 """
 """
-
+import os.path
 from logging import getLogger
 
 import numpy as np
@@ -8,10 +8,58 @@ import numpy as np
 logger = getLogger(__name__)
 
 
+def _get_leff(path_leff, l_files):
+    """Return dictionary with 3 antenna Leff
+
+    """
+    path_ant = os.path.join(path_leff, l_files[0])
+    leff_ew = AntennaLeffStorage()
+    leff_ew.name = "EW"
+    leff_ew.load(path_ant)
+    path_ant = os.path.join(path_leff, l_files[1])
+    leff_sn = AntennaLeffStorage()
+    leff_sn.load(path_ant)
+    leff_sn.name = "SN"
+    path_ant = os.path.join(path_leff, l_files[2])
+    leff_up = AntennaLeffStorage()
+    leff_up.load(path_ant)
+    leff_up.name = "UP"
+    d_leff = {"sn": leff_sn, "ew": leff_ew, "up": leff_up}
+    return d_leff
+
+
+def get_leff_default(path_leff):
+    """Return dictionary with 3 antenna Leff
+
+    :param path_leff: path to file Leff
+    :type path_leff: string
+    """
+    l_files = [
+        "Light_GP300Antenna_EWarm_leff.npz",
+        "Light_GP300Antenna_SNarm_leff.npz",
+        "Light_GP300Antenna_Zarm_leff.npz",
+    ]
+    return _get_leff(path_leff, l_files)
+
+
+def get_leff_nec(path_leff):
+    """Return dictionary with 3 antenna Leff
+
+    :param path_leff: path to file Leff
+    :type path_leff: string
+    """
+    l_files = [
+        "Light_GP300Antenna_nec_EWarm_leff.npz",
+        "Light_GP300Antenna_nec_SNarm_leff.npz",
+        "Light_GP300Antenna_nec_Zarm_leff.npz",
+    ]
+    return _get_leff(path_leff, l_files)
+
+
 class AntennaLeffStorage:
     """
     Angle convention
-        phi : 0 for nord, 90 for west
+        phi : 0 for north, 90 for west
         theta :O for up, 90 for ground
     """
 
