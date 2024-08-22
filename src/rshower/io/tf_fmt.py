edf407e74097c1c6a12c799b4af25c48feff794a
luckyjim <jcolley@lpnhe.in2p3.fr>
2024-08-22 11:54:58 +0200

    define file name of RF chain

--- src/rshower/io/tf_fmt.py
+++ src/rshower/io/tf_fmt.py
@@ -3,11 +3,13 @@ Global RF chain transfer function
 """
 
 import os.path
+
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def read_one_column(pn_file, f_0_mhz, step_mhz):
-    tfc = np.loadtxt(pn_file)
+    tfc = np.loadtxt(pn_file, dtype=np.complex128)
     assert tfc.ndim == 1
     freq = f_0_mhz + np.arange(tfc.shape[0]) * step_mhz
     return freq, tfc
@@ -30,7 +32,7 @@ def read_TFx_yy_fmt(n_path, n_ew, n_ns, n_z):
 
 def read_TF2_fmt(n_path):
     """
-    like /sps/grand/snonis/TFSparam/TF3_20db_NS
+    
     """
     n_ew = "TF2_20db_EW"
     n_ns = "TF2_20db_NS"
@@ -40,9 +42,26 @@ def read_TF2_fmt(n_path):
 
 def read_TF3_fmt(n_path):
     """
-    like /sps/grand/snonis/TFSparam/TF3_20db_NS
+    
     """
-    n_ew = "TF3_20db_EW"
-    n_ns = "TF3_20db_NS"
-    n_z = "TF3_20db_Z"
+    n_ew = "TF3_20dB_EW"
+    n_ns = "TF3_20dB_NS"
+    n_z = "TF3_20dB_Z"
     return read_TFx_yy_fmt(n_path, n_ew, n_ns, n_z)
+
+
+def plot_global_rf_chain(n_path, f_read, title=""):
+    freq, tf_ew, tf_ns, tf_z = f_read(n_path)
+    plt.figure()
+    plt.title(title)
+    plt.plot(freq, np.abs(tf_ns), "k", label="SN")
+    plt.plot(freq, np.abs(tf_ew), "y", label="EW")
+    plt.plot(freq, np.abs(tf_z), "b", label="UP")
+    plt.legend()
+    plt.grid()
+    plt.xlabel("MHz")
+    plt.ylabel("dB")
+
+
+def plot_global_rf_chain_TF3(n_path, title="RF chain version TF3"):
+    plot_global_rf_chain(n_path, read_TF3_fmt, "RF chain version TF3")
