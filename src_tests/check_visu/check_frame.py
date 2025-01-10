0d03bcf7d6b889cbfd649e390a4de6a0b87702b1
luckyjim <jcolley@lpnhe.in2p3.fr>
2025-01-10 17:37:18 +0100

    example of circulatisation

--- src_tests/check_visu/check_frame.py
+++ src_tests/check_visu/check_frame.py
@@ -11,19 +11,42 @@ from rshower.io.events.grand_io_fmt import convert_3dtrace_grandlib
 from rshower.basis.frame import *
 from rshower.basis.efield_event import HandlingEfield
 from rshower.basis.traces_event import Handling3dTraces
+from numba.core.types import none
 
 
 P_dc2 = "/home/jcolley/projet/grand_wk/data/root/dc2/ZHAires-NJ/sim_Xiaodushan_20221025_220000_RUN0_CD_ZHAireS_0000/"
 F_efield = "efield_29-24992_L0_0000.root"
 
+
 def check_dc2(i_e):
     tref_gl = froot.get_handling3dtraces(P_dc2 + F_efield, i_e)
     d_simu = froot.get_simu_parameters(P_dc2 + F_efield, i_e)
     tref = convert_3dtrace_grandlib(tref_gl, True)
+    print(d_simu)
+    tref.set_xmax(d_simu["FIX_xmax_pos"])
+    tref.network.core_pos = d_simu["shower_core_pos"]
     assert isinstance(tref, HandlingEfield)
     tref.plot_footprint_val_max()
-    
-    
+    trshw = FrameNetFrameShower()
+    zen = d_simu["zenith"]
+    azi = d_simu["azimuth"]
+    print(zen, azi)
+    dir_x = coord.nwu_sph_to_cart(np.array([np.deg2rad(azi), np.deg2rad(zen), 1.0]))
+    trshw.init_v_inc(-dir_x, np.deg2rad(90), d_simu["FIX_xmax_pos"])
+    print(-dir_x)
+    #trshw.init_v_inc(-dir_x, 0)
+    print(tref.network.du_pos.shape)
+    pos_sh = trshw.pos_to(tref.network.du_pos.T, "SHW")
+    print(pos_sh.shape)
+    print(pos_sh.T[:3])
+    tref_sh = tref.copy()
+    tref_sh.network.du_pos = pos_sh.T
+    tref_sh.network.xmax_pos = None
+    tref_sh.network.core_pos = None
+    tref_sh.axis_name = ["vxB", "vx(vxB)", "v"]
+    tref_sh.plot_footprint_val_max()
+
+
 def check_frame_to_shower():
     aa = np.linspace(0, 2 * 3.14159, 20)
     elp2 = np.array([20 * np.cos(aa), 50 * np.sin(aa)])
@@ -51,6 +74,7 @@ def check_frame_to_shower():
     plt.axis("equal")
     plt.grid()
 
+
 def check_frame_shower2net():
     aa = np.linspace(0, 2 * 3.14159, 20)
     elp2 = np.array([50 * np.cos(aa), 50 * np.sin(aa)])
@@ -75,8 +99,9 @@ def check_frame_shower2net():
     plt.axis("equal")
     plt.grid()
 
+
 if __name__ == "__main__":
     # check_frame_to_shower()
     # check_frame_shower2net()
-    check_dc2(318)
+    check_dc2(314)
     plt.show()
