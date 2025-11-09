0929ccf3f2d7d54c97959d1af781a4d2533a64d2
luckyjim <jcolley@lpnhe.in2p3.fr>
2025-11-09 23:52:41 +0100

    update vers

--- src_lib/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-Radio shower package
-"""
-__version__ = "0.0.1"
-__author__ = "Colley Jean-Marc"
--- src_lib/rshower/__init__.py
+++ src_lib/rshower/__init__.py
@@ -1,16 +1,5 @@
 """
 Radio shower package
 """
-__version__ = "0.1.1"
+__version__ = "0.0.2"
 __author__ = "Colley Jean-Marc"
-
-PATH_MODEL = ""
-
-
-def set_path_model_du(s_path):
-    global PATH_MODEL
-    PATH_MODEL = s_path
-
-
-def get_path_model_du():
-    return PATH_MODEL
