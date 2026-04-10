a74fe3e999b07c08576b9a49596667c98247d7c4
luckyjim <jcolley@lpnhe.in2p3.fr>
2026-04-10 13:49:04 +0200

    unformate

--- src_lib/rshower/basis/traces_event.py
+++ src_lib/rshower/basis/traces_event.py
@@ -4,6 +4,9 @@ Colley Jean-Marc, CNRS/IN2P3/LPNHE
 Handling a set of 3D traces
 """
 
+from matplotlib import colors
+from matplotlib.backend_bases import MouseButton
+from matplotlib.widgets import CheckButtons
 import copy
 from logging import getLogger
 
@@ -11,9 +14,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import rshower.num.signal as rss
 import scipy.signal as ssig
-from matplotlib import colors
-from matplotlib.backend_bases import MouseButton
-from matplotlib.widgets import CheckButtons
 from rshower.basis.du_network import DetectorUnitNetwork
 
 logger = getLogger(__name__)
